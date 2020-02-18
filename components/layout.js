bb7b6f8ab9018bc4782317acf0291f2654621634
Bekir Cem Ünalan <unalanbekircem@gmail.com>
2020-02-18 18:47:32 +0300

    Import Head

--- components/layout.js
+++ components/layout.js
@@ -1,17 +1,18 @@
 import Navigation from './navigation'
+import Head from 'next/head'
 
-function Layout({children}) {
-
-    return <div>
-        <Navigation/>
-        <main>
-            {children}
-        </main>
-        <footer>
-            design by cem
-        </footer>
+function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Sample</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Navigation />
+      <main>{children}</main>
+      <footer>design by cem</footer>
     </div>
-
+  )
 }
 
 export default Layout
